670c355267240ce48605ed2c5111ff0e68f6737d
Verenich Tatiana <verenik91@gmail.com>
2023-03-07 17:22:00 +0300

    добавлена задача 2

---
+++ examples_002/Program.cs
@@ -0,0 +1,7 @@
+﻿// начало, ввод username, вывод "привет, ", вывод username, конец.
+Console.WriteLine("Введите Ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+
+
