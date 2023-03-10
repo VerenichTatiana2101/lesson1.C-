286dd8878fdbf10658890136f750ade3f96b6fee
Verenich Tatiana <verenik91@gmail.com>
2023-03-10 10:00:37 +0300

    добавила файл 2

---
+++ examples_202/Program.cs
@@ -0,0 +1,14 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int[] array = { 1, 28, 63, 34, 15, 61, 7, 85, 9 };
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+Console.WriteLine(max);
