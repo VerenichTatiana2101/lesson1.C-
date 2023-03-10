124ffd347af64aa91f11f55af987706f46b83f56
Verenich Tatiana <verenik91@gmail.com>
2023-03-10 22:04:56 +0300

    Поиск макс из 9 чисел

--- examples_202/Program.cs
+++ examples_202/Program.cs
@@ -1,14 +1,19 @@
-﻿int Max(int arg1, int arg2, int arg3)
+﻿///Выводим наибольшее число из массива из 9 чисел
+
+
+int Max(int arg1, int arg2, int arg3)
 {
     int result = arg1;
     if (arg2 > result) result = arg2;
     if (arg3 > result) result = arg3;
     return result;
 }
-int[] array = { 1, 28, 63, 34, 15, 61, 7, 85, 9 };
+              //0  1   2    3   4   5  6   7   8
+int[] array = { 11, 28, 63, 34, 95, 61, 7, 85, 9 };
 
-int max = Max(
-    Max(a1, b1, c1),
-    Max(a2, b2, c2),
-    Max(a3, b3, c3));
-Console.WriteLine(max);
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
