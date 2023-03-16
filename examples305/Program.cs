8f51a1d4378a0a370f96090990f832cf3a43eb20
Verenich Tatiana <verenik91@gmail.com>
2023-03-17 00:21:29 +0300

    Добавлена работа с циклом for

---
+++ examples305/Program.cs
@@ -0,0 +1,15 @@
+﻿// циклы не только while
+//for (цикл со счётчиком)
+// 
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++) //сначало инициализация счётчика, далее проверка условия, инкримент i++
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "z");
+Console.WriteLine(res);
