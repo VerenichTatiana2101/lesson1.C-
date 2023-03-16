4c2d42dce143949e6b8a8883fc8d059b0d521987
Verenich Tatiana <verenik91@gmail.com>
2023-03-17 01:50:58 +0300

    Добавлена часть

---
+++ examples_308/Program.cs
@@ -0,0 +1,16 @@
+﻿// Упорядочить массив (данные внутри массива)
+// алгоритм сортировки методом выбора (метод мини макс-а)
+
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(arr);
