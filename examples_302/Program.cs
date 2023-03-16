d5b3499e8e24d35817bae3d068f1f00ee458c72c
Verenich Tatiana <verenik91@gmail.com>
2023-03-17 00:06:04 +0300

    Добавлены 4 вида методов

---
+++ examples_302/Program.cs
@@ -0,0 +1,24 @@
+﻿// Второй вид метода
+// что-то принимают
+// ничего не возвращают.
+// void метод
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine(mag);
+// }
+// Method2("Текст сообщения");
+
+void Method2(string msg, int count)
+{
+    int i = 0;
+    while(i< count)
+    {
+        Console.WriteLine(msg);
+        i ++;
+    }
+    
+}
+Method2(msg: "Текст", count: 4); //пишет слово текст 4 раза(первый 
+                    // аргумент msg "Текст", второй count введён 4)
+//с указанием аргументов можно вписывать в вызов в разном порядке
---
+++ examples_304/Program.cs
@@ -0,0 +1,20 @@
+﻿// Четвёртый вид метода
+// что-то принимает
+// что-то возвращает
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty; //резалт у нас изначально будет пустой строкой
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "z"); //склеивает 10 z
+Console.WriteLine(res);
+
+
