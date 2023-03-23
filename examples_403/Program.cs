d47b89650bd768d50617ec931aa4047a45a2e371
Verenich Tatiana <verenik91@gmail.com>
2023-03-24 00:12:15 +0300

    факториал и рекурсия

---
+++ examples_403/Program.cs
@@ -0,0 +1,14 @@
+﻿// факториал и рекурсия и переполнение
+
+double Factorial(int n)
+{
+    if (n == 1) return 1;    //1! = 1  0! = 1
+    else return n * Factorial(n - 1);
+}
+
+// Console.WriteLine(Factorial(3));   //1*2*3
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! {Factorial(i)}");
+}
