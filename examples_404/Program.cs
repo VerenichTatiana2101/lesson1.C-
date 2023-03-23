2c94c63e3f56caff4358004a80da5797c6d67f8c
Verenich Tatiana <verenik91@gmail.com>
2023-03-24 00:46:47 +0300

    задача Фибоначчи

---
+++ examples_404/Program.cs
@@ -0,0 +1,13 @@
+﻿// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(f-2)
+
+double Fiboacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fiboacci(n-1) + Fiboacci(n-2);
+}
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f{i} = {Fiboacci(i)}");
+}
